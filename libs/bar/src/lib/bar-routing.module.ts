13c4c2508282cf14d197f6043dc163146bdfe031
Rares Matei <matei.rar@gmail.com>
2021-04-19 14:20:48 +0100

    replicate lazy loading issue

--- libs/bar/src/lib/bar-routing.module.ts
+++ libs/bar/src/lib/bar-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { BarComponent } from "./bar/bar.component";
 
 export const routes: Routes = [
-  { path: '', component: BarComponent },
   { path: 'baz', loadChildren: () => import('@incrementalbuildlazyloading/baz').then(m => m.BazModule) },
+  { path: 'bar-inner', loadChildren: () => import('./bar/bar.component.module').then(m => m.BarInnerModule) },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  declarations: [BarComponent]
 })
 export class BarRoutingModule {}
